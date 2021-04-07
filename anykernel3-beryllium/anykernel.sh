a6674e2497ee60e1b64fa89f272ab7b2e052c96f
Martin Valba <kailing2@live.com>
2021-04-07 15:46:30 -0400

    could be a mess but well see
    All of my commits will come this way even if they started nicely

--- anykernel3-beryllium/anykernel.sh
+++ anykernel3-beryllium/anykernel.sh
@@ -29,9 +29,6 @@ ramdisk_compression=auto;
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
-## Importing hcore
-#. hcore/hcore.sh;
-
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
 set_perm_recursive 0 0 755 644 $ramdisk/*;
@@ -42,12 +39,6 @@ set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 dump_boot;
 
 write_boot;
-
-#
-# Hill-core part (UNFINISHED DO NOT ENABLE THIS!!!)
-#
-
-#hcore
 
 # END OF HCORE
 
